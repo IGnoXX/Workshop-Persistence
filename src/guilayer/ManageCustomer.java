112536c2b67c76d8adaf1a95d10cc55bd349abdf
flawlessBadger <jozefferko9@gmail.com>
2018-03-20 10:54:13 +0100

    removeMain

--- src/guilayer/ManageCustomer.java
+++ src/guilayer/ManageCustomer.java
@@ -73,18 +73,18 @@ public class ManageCustomer extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ManageCustomer frame = new ManageCustomer();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					ManageCustomer frame = new ManageCustomer();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
